1d41737520b3f54897e64f14dd819d09d8b50151
Jim DiNunzio <jim@dinunzio.com>
2023-07-31 23:52:56 -0700

    [Riva ASR] added transcribe mic to chatlog program which prints a line of text every time a long enough pause in dialog occurs.

---
+++ scripts/asr/trans_mic_to_chatlog.bat
@@ -0,0 +1,1 @@
+python transcribe_mic_to_chatlog.py --server 192.168.1.41:50051 --input-device 1 --chatlog-role User --chatlog-file "C:\Users\jim\Desktop\Cooper\Windows\MetahumanChatbot\Saved\meeting_transcript.txt" --system-prompt-file "C:\Users\jim\Desktop\Cooper\Windows\MetahumanChatbot\Saved\chat_system_prompt.txt"
